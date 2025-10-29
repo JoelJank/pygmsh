bccef7bf8eb23c804da17636f78ccd131a09d6b7
JoelJank <joel.jankowiak@stud.uni-due.de>
2025-10-29 23:58:16 +0100

    Added settings and calculation for inflation layer

--- test.py
+++ test.py
@@ -8,6 +8,50 @@ import numpy as np
 
 settings_path = "settings/settings.json"
 
+def layercalcuations(hparts, growthrate, h1):
+    nlayer = math.log(1-(hparts*(1-growthrate)/h1))/math.log(growthrate)
+    nextlayer = h1 * growthrate**nlayer
+    lastlayer = h1 * growthrate**(nlayer-1)
+    return nlayer, nextlayer, lastlayer
+
+def totalheightcalculation(h1, growthrate, nlayers):
+    totalheight = h1* (1- growthrate**nlayers) / (1 - growthrate)
+    return totalheight
+
+
+def inflationcalculation(h1, growthrate, nlayers, hFence, nparts):
+    heightparts = hFence /nparts
+    meshdata = np.empty((nparts+1,2), dtype = object)
+    totalheight = totalheightcalculation(h1, growthrate, nlayers)
+    nFirstlayer, nextlayer, lastlayer = layercalcuations(heightparts, growthrate, h1)
+    if h1 > heightparts: #wenn h1 direkt größer als Zaunhöhe ist
+        for i in range(0,nparts):
+            meshdata[i] = [1, heightparts*(1+i)]
+    else: #sonst normale berechnung
+        meshdata[0] = [math.ceil(nFirstlayer), lastlayer]
+        for i in range(1,nparts):
+            if nextlayer > heightparts:
+                nextlayer = lastlayer + heightparts
+                meshdata[i] = [1, nextlayer]
+                lastlayer = nextlayer
+            else:
+                nlayer, nextlayer, lastlayer = layercalcuations(heightparts, growthrate, nextlayer)
+                meshdata[i] = [math.ceil(nlayer), lastlayer]
+                
+    sumlayers = np.sum(meshdata[:-1,0])
+    remaininglayer = nlayers - sumlayers
+    toppoints = meshdata[-2][1]
+    if remaininglayer > 0:
+        heightlastlayer = totalheightcalculation(nextlayer, growthrate, remaininglayer)
+        toppoints = heightlastlayer + nparts*heightparts
+        meshdata[-1] = [remaininglayer, heightlastlayer + meshdata[-2][1]]
+    else:
+        meshdata = meshdata[:-1]
+        
+    return meshdata, toppoints, abs(toppoints - totalheight)
+
+
+    
 with open(settings_path, 'r') as f:
     settings = json.load(f)
 
@@ -20,7 +64,13 @@ lChannel = settings["width_of_channel"]
 xfirstfence = settings["xpos_firstfence"]
 resolution = settings["mesh_resolution"]
 meshFreesize = settings["mesh_freesize"]
+meshFirstlayerheight = settings["mesh_firstlayerheight"]
+meshGrowthrate = settings["mesh_growthrate"]
+meshNumberofinflationlayers = settings["inflation_layers"]
 
+meshdata, toppoints, diff = inflationcalculation(meshFirstlayerheight, meshGrowthrate, meshNumberofinflationlayers, hFences, nSlits)
+
+print(f"Meshdata for inflation layers: {meshdata} \nTop points: {toppoints} \nDifferenz to calculated inflation height: {diff}")
 # Checks on parameters
 if hChannel > 10*hFences:
     hChannel = 10*hFences
@@ -30,6 +80,7 @@ if lChannel < neededwidth:
     lChannel = neededwidth
 
 
+
 #Calculations:
 
 #xpositions of fences:
@@ -37,7 +88,7 @@ xposFences = [xfirstfence + i*dxFences for i in range(nFences)]
 
 #ypositions of slits in fences:
 if nSlits > 1:
-    dySlits = hFences/(nSlits)
+    dySlits = hFences/nSlits
     yposSlits = [j*dySlits for j in range(1,nSlits+1)]
 else:
     yposSlits = [hFences]
@@ -49,11 +100,12 @@ vertical_lines = np.empty((nFences+2,nSlits+1), dtype = object)
 horizontal_lines = np.empty((2,nFences+1), dtype = object)
 plane_loops = np.empty((nFences+1), dtype = object)
 plane_surfaces = np.empty((nFences+1), dtype= object)
+mesh_inflation = np.empty((nFences+1), dtype = object)
 
 
 #calculations for meshing
 
-nFreebeforfirstfence = math.ceil(xfirstfence/meshFreesize)+1 #+1 because gmsh is dumb lol
+nFreebeforfirstfence = math.ceil(xfirstfence/meshFreesize)+1 
 nBetweenfences = math.ceil(dxFences/meshFreesize)+1
 nAbovefences = math.ceil((hChannel - hFences)/meshFreesize)+1
 
@@ -106,7 +158,7 @@ for i in range(nFences+1):
 
 
 #Meshing
-#For cures at the start and end of the channel:
+#For curves at the start and end of the channel:
 m.set_transfinite_curve(horizontal_lines[0][0], nFreebeforfirstfence, "Progression", 1.0)
 m.set_transfinite_curve(horizontal_lines[0][-1], nFreebeforfirstfence, "Progression", 1.0)
 m.set_transfinite_curve(horizontal_lines[1][0], nFreebeforfirstfence, "Progression", 1.0)
@@ -120,7 +172,7 @@ for i in range(1,nFences):
 #For curves above the fences + for the curves of the fences
 
 for i in range(len(vertical_lines)):
-    m.set_transfinite_curve(vertical_lines[i][-1], nAbovefences,"Progression", 1.0)
+    m.set_transfinite_curve(vertical_lines[i][-1], nAbovefences,"Progression", 1.1)
     for j in range(len(vertical_lines[i])-1):
         m.set_transfinite_curve(vertical_lines[i][j],10,"Progression", 1.0) 
     #here: do the adjustments for the boundary layer meshing!
@@ -147,6 +199,6 @@ for i in range(1,nFences+1):
         m.add_physical(vertical_lines[i][j], f"z{i}s{j+1}")
 
 geo.generate_mesh(dim=2)
-gmsh.write("test3.msh")
+gmsh.write("test3.bdf")
 geo.__exit__()
 

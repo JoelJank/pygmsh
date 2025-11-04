a105c03b2c271d8d5bffe94ec50a55334159d826
JoelJank <joel.jankowiak@stud.uni-due.de>
2025-11-04 22:57:32 +0100

    Cleared functions

--- pygmsh/pygmsh_2D.py
+++ pygmsh/pygmsh_2D.py
@@ -1,79 +1,29 @@
 import pygmsh
-import meshio
 import gmsh
-import json
 import math
-import os
 import numpy as np
+from utils.meshcalc import inflationcalculation
+from utils.json import json_read
 
-settings_path = "settings/settings.json"
-
-def layercalcuations(hparts, growthrate, h1):
-    nlayer = math.log(1-(hparts*(1-growthrate)/h1))/math.log(growthrate)
-    nextlayer = h1 * growthrate**nlayer
-    lastlayer = h1 * growthrate**(nlayer-1)
-    return nlayer, nextlayer, lastlayer
-
-def totalheightcalculation(h1, growthrate, nlayers):
-    totalheight = h1* (1- growthrate**nlayers) / (1 - growthrate)
-    return totalheight
-
-
-
-
-
-def inflationcalculation(h1, growthrate, nlayers, hFence, nparts, hChannel, growthaferinflation):
-    heightparts = hFence /nparts
-    meshdata = np.empty((nparts+1,2), dtype = object)
-    totalheight = totalheightcalculation(h1, growthrate, nlayers)
-    nFirstlayer, nextlayer, lastlayer = layercalcuations(heightparts, growthrate, h1)
-    if h1 > heightparts: #wenn h1 direkt größer als Zaunhöhe ist
-        for i in range(0,nparts):
-            meshdata[i] = [1, heightparts*(1+i)]
-    else: #sonst normale berechnung
-        meshdata[0] = [math.ceil(nFirstlayer), lastlayer]
-        for i in range(1,nparts):
-            if nextlayer > heightparts:
-                nextlayer = heightparts
-                meshdata[i] = [1, nextlayer]
-                lastlayer = nextlayer
-            else:
-                nlayer, nextlayer, lastlayer = layercalcuations(heightparts, growthrate, nextlayer)
-                meshdata[i] = [math.ceil(nlayer), lastlayer]
-                
-    sumlayers = np.sum(meshdata[:-1,0]) #auffüllen der restlichen Layer
-    remaininglayer = nlayers - sumlayers
-    if remaininglayer > 0:
-        heightlastlayer = totalheightcalculation(nextlayer, growthrate, remaininglayer)
-        nlayer, nextlayer, lastlayer = layercalcuations(heightlastlayer, growthrate, nextlayer)
-        toppoints = heightlastlayer + hFence
-        meshdata[-1] = [remaininglayer, lastlayer]
-    else:
-        toppoints = nparts*heightparts + meshdata[-2][1]
-        meshdata[-1] = [1, lastlayer]
-        
-    #für inflation bis nach oben
-    nbisoben, nextlayeroben, lastlayeroben = layercalcuations(hChannel-toppoints, growthaferinflation, lastlayer)
-    return meshdata, toppoints, [math.ceil(nbisoben), lastlayeroben]
-
-
+settings_path = "../config/settings.json"
     
-with open(settings_path, 'r') as f:
-    settings = json.load(f)
+settings = json_read(settings_path)
 
-nFences =  settings["number_of_fences"]
-hFences = settings["height_of_fences"]
-dxFences = settings["distance_of_fences"]
-nSlits = settings["number_of_slits"]
-hChannel = settings["height_of_channel"]
-lChannel = settings["width_of_channel"]
-xfirstfence = settings["xpos_firstfence"]
-resolution = settings["mesh_resolution"]
-meshFreesize = settings["mesh_freesize"]
-meshFirstlayerheight = settings["mesh_firstlayerheight"]
-meshGrowthrate = settings["mesh_growthrate"]
-meshNumberofinflationlayers = settings["inflation_layers"]
-meshgrowthafterinflation = settings["meshgrowtrate_afterinflation"]
+nFences =  settings["nFences"]
+hFences = settings["hFences"]
+dxFences = settings["dxFences"]
+nSlits = settings["nSlits"]
+hChannel = settings["hChannel"]
+lChannel = settings["lChannel"]
+xfirstfence = settings["xfirstfence"]
+resolution = settings["resolution"]
+meshFreesize = settings["meshFreesize"]
+meshFirstlayerheight = settings["meshFirstlayerheight"]
+meshGrowthrate = settings["meshGrowthrate"]
+meshNumberofinflationlayers = settings["meshNumberofinflationlayers"]
+meshgrowthafterinflation = settings["meshgrowthafterinflation"]
+savespace = settings["savespace"]
+
 
 neededwidth = 2* xfirstfence + (nFences-1)*dxFences # make safe that all fences fit in channel and that inlet is the same width as outlet
 if lChannel < neededwidth:
@@ -206,6 +156,6 @@ for i in range(1,nFences+1):
         m.add_physical(vertical_lines[i][j], f"z{i}s{j+1}")
 
 geo.generate_mesh(dim=2)
-gmsh.write("mesh.msh")
+gmsh.write(savespace)
 geo.__exit__()
 
---
+++ pygmsh/utils/meshcalc.py
@@ -0,0 +1,49 @@
+import numpy as np
+import math
+
+
+
+def layercalcuations(hparts, growthrate, h1):
+    nlayer = math.log(1-(hparts*(1-growthrate)/h1))/math.log(growthrate)
+    nextlayer = h1 * growthrate**nlayer
+    lastlayer = h1 * growthrate**(nlayer-1)
+    return nlayer, nextlayer, lastlayer
+
+def totalheightcalculation(h1, growthrate, nlayers):
+    totalheight = h1* (1- growthrate**nlayers) / (1 - growthrate)
+    return totalheight
+
+
+def inflationcalculation(h1, growthrate, nlayers, hFence, nparts, hChannel, growthaferinflation):
+    heightparts = hFence /nparts
+    meshdata = np.empty((nparts+1,2), dtype = object)
+    totalheight = totalheightcalculation(h1, growthrate, nlayers)
+    nFirstlayer, nextlayer, lastlayer = layercalcuations(heightparts, growthrate, h1)
+    if h1 > heightparts: #wenn h1 direkt größer als Zaunhöhe ist
+        for i in range(0,nparts):
+            meshdata[i] = [1, heightparts*(1+i)]
+    else: #sonst normale berechnung
+        meshdata[0] = [math.ceil(nFirstlayer), lastlayer]
+        for i in range(1,nparts):
+            if nextlayer > heightparts:
+                nextlayer = heightparts
+                meshdata[i] = [1, nextlayer]
+                lastlayer = nextlayer
+            else:
+                nlayer, nextlayer, lastlayer = layercalcuations(heightparts, growthrate, nextlayer)
+                meshdata[i] = [math.ceil(nlayer), lastlayer]
+                
+    sumlayers = np.sum(meshdata[:-1,0]) #auffüllen der restlichen Layer
+    remaininglayer = nlayers - sumlayers
+    if remaininglayer > 0:
+        heightlastlayer = totalheightcalculation(nextlayer, growthrate, remaininglayer)
+        nlayer, nextlayer, lastlayer = layercalcuations(heightlastlayer, growthrate, nextlayer)
+        toppoints = heightlastlayer + hFence
+        meshdata[-1] = [remaininglayer, lastlayer]
+    else:
+        toppoints = nparts*heightparts + meshdata[-2][1]
+        meshdata[-1] = [1, lastlayer]
+        
+    #für inflation bis nach oben
+    nbisoben, nextlayeroben, lastlayeroben = layercalcuations(hChannel-toppoints, growthaferinflation, lastlayer)
+    return meshdata, toppoints, [math.ceil(nbisoben), lastlayeroben]
